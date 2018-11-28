18a78e5d1d6fe6a4e20eee653f551a2781936638
xrotwang <xrotwang@googlemail.com>
2018-11-28 08:48:43 +0100

    release 1.0.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import codecs
 
 setup(
     name='pyclts',
-    version="0.2.2.dev0",
+    version="1.0.0",
     description='A python library to check phonetic transcriptions',
     long_description=codecs.open("README.md", 'r', 'utf-8').read(),
     long_description_content_type='text/markdown',
