9763328bd66b3aa2fc8f4e3b0c055d18a5a21b78
xrotwang <xrotwang@googlemail.com>
2019-09-18 18:19:40 +0200

    bump version for development

--- src/pyclts/__init__.py
+++ src/pyclts/__init__.py
@@ -2,4 +2,4 @@ from pyclts.transcriptionsystem import TranscriptionSystem  # noqa: F401
 from pyclts.transcriptiondata import TranscriptionData  # noqa: F401
 from pyclts.soundclasses import SoundClasses  # noqa: F401
 
-__version__ = '1.3.0'
+__version__ = '1.3.1.dev0'
