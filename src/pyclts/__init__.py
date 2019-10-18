467853d4af57a3ddb3a6d544e88c1bf89976c3bc
lingulist <mattis.list@lingpy.org>
2019-10-18 14:33:03 +0200

    import api explicitly

--- src/pyclts/__init__.py
+++ src/pyclts/__init__.py
@@ -1,5 +1,6 @@
 from pyclts.transcriptionsystem import TranscriptionSystem  # noqa: F401
 from pyclts.transcriptiondata import TranscriptionData  # noqa: F401
 from pyclts.soundclasses import SoundClasses  # noqa: F401
+from pyclts import api
 
 __version__ = '1.3.1.dev0'
